7f2e76e4a60e91d728f995d8b686b94ecde27982
filimonadrian <adrian.filimon@hotmail.com>
2021-04-29 17:24:39 +0300

    Added skel and solved partially neoptimized method

---
+++ skel/solver_neopt.c
@@ -0,0 +1,67 @@
+/*
+ * Tema 2 ASC
+ * 2021 Spring
+ */
+#include "utils.h"
+
+/*
+ * Add your unoptimized implementation here
+ */
+
+
+void allocate_matrix(int N, double **AB, double **ABBt,
+                        double **AtA, double **C) {
+
+    *AB = calloc (N * N, sizeof (double));
+    *ABBt = calloc (N * N, sizeof (double));
+    *AtA = calloc (N * N, sizeof (double));
+    *C = calloc (N * N, sizeof (double));
+}
+
+double *my_solver(int N, double *A, double* B) {
+
+    double *AB, *ABBt, *AtA, *C;
+    int i, j, k;
+
+    allocate_matrix(N, &AB, &ABBt, &AtA, &C);
+
+    /* A * B*/
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            for (k = j; k < N; k++) {
+                AB[i * N + j] += A[i * N + k] * B[k * N + j];
+            }
+        }
+    }
+
+    /* AB * B transpose */
+    for (i = 0; i < N; i++) {
+        for (j = i; j < N; j++) {
+            for (k = i; k < N; k++) {
+                ABBt[i * N + j] += AB[i * N + k] * B[j * N + k];
+            }
+        }
+    }
+
+    /* A transpose * A */
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            for (k = 0; k < N; k++) {
+                AtA[i * N + j] += A[i * N + k] * A[j * N + k];
+            }
+        }
+    }
+    /* C = */
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            C[i * N + j] += ABBt[i * N + j] + AtA[i * N + j];
+        }
+    }
+
+    free(AB);
+    free(ABBt);
+    free(AtA);
+    free(C);
+
+    return C;
+}
