2c1863ef2562b367a09c3ddd94115cde4f5d46e4
filimonadrian <adrian.filimon@hotmail.com>
2021-05-02 15:35:41 +0300

    neopt fault

--- skel/solver_neopt.c
+++ skel/solver_neopt.c
@@ -56,7 +56,7 @@ double *my_solver(int N, double *A, double* B) {
 
     k = 0;
     for (i = 0; i < N; i++) {
-        for (j = k; j < N; j++) {
+        for (j = 0; j < N; j++) {
             for (k = 0; k < N; k++) {
                 AtA[i * N + j] += A[k * N + i] * A[k * N + j];
             }
