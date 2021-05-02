f592c044cf2f418b6dbdb992c82659c98f920967
filimonadrian <adrian.filimon@hotmail.com>
2021-05-02 14:36:55 +0300

    Added script for running remote

---
+++ skel/comp_out.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./compare /export/asc/tema2/out1 out1 0.0001
+./compare /export/asc/tema2/out2 out2 0.0001
+./compare /export/asc/tema2/out3 out3 0.0001
---
+++ skel/run_queue.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm out1 out2 out3
+rm run_tema2.sh.*
+
+qsub -cwd -q ibm-nehalem.q run_tema2.sh
+
