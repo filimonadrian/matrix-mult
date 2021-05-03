c576a52847b36dbb1205238828e3c60c4b9167e3
filimonadrian <adrian.filimon@hotmail.com>
2021-05-03 14:55:33 +0300

    Modified scripts for run

--- skel/run_queue.sh
+++ skel/run_queue.sh
@@ -4,4 +4,4 @@ rm out1 out2 out3
 rm run_tema2.sh.*
 
 qsub -cwd -q ibm-nehalem.q run_tema2.sh
-
+# qsub -cwd -q ibm-nehalem.q cachegrind.sh
--- skel/run_tema2.sh
+++ skel/run_tema2.sh
@@ -4,9 +4,14 @@ module load compilers/gnu-5.4.0
 
 make
 
-# ./tema2_neopt /export/asc/tema2/input
-./tema2_opt_m /export/asc/tema2/input
-# ./tema2_blas /export/asc/tema2/input
+ ./tema2_neopt /export/asc/tema2/input
+#  ./tema2_opt_m /export/asc/tema2/input
+#  ./tema2_blas /export/asc/tema2/input
 
-make clean
+# values for graph
+# ./tema2_neopt input
+# ./tema2_opt_m input
+# ./tema2_blas input
+
+# make clean
 
